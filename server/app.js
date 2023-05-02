43f6da22789d139bf16ab5b6f619b53e4d873546
jc <huangjingchao0127@gmail.com>
2023-05-02 23:51:33 +0800

    #1

---
+++ server/app.js
@@ -0,0 +1,26 @@
+const Koa = require('koa');
+const app = new Koa();
+
+const main = ctx => {
+  ctx.response.body = {
+    code: 1,
+    message: '请求成功',
+    data: [
+      {
+        name: 'kobe',
+        age: '24'
+      },
+      {
+        name: 'durant',
+        age: '35'
+      },
+      {
+        name: 'james',
+        age: '6'
+      }
+    ]
+  };
+};
+
+app.use(main);
+app.listen(3000)
